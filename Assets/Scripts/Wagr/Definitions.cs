610b5d541a69c7b93cf00a9d85a144bab1cee155
Oyale <salamivictor15@gmail.com>
2025-11-13 14:55:34 +0100

    Adds TextMeshPro to the document

---
+++ Assets/Scripts/Wagr/Definitions.cs
@@ -0,0 +1,21 @@
+namespace Wagr
+{
+    public class Profile
+    {
+        private int uid;
+        private string username;
+
+        public Profile()
+        {
+
+        }
+    }
+
+    public enum ChatType : byte
+    {
+    }
+
+    public enum ActionType : byte 
+    {
+    }
+}
