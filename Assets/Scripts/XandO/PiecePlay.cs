52cb2d881e78b4d3ccd99453229f437653ebbe1b
Oyale <salamivictor15@gmail.com>
2025-12-18 00:46:27 +0100

    Some work in the XandO part

---
+++ Assets/Scripts/XandO/PiecePlay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePlay : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("Pressed");
+    }
+}
