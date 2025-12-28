82ccc03f13ff0dd67045a229a79c37f9b5748f9e
Oyale <salamivictor15@gmail.com>
2025-12-28 01:34:04 +0100

    Cleanes up UI and added the Notification Display feature

---
+++ Assets/Scripts/UI Handlers/NotificationDisplay.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+
+public class NotificationDisplay : MonoBehaviour
+{
+    /// <summary> A single instance of the Notification Bar </summary>
+    public static NotificationDisplay instance;
+
+    [SerializeField] private TMP_Text notificationContent;
+
+    public void DisplayMessage(string text, NotificationType type = NotificationType.info, float time = -1)
+    {
+        gameObject.SetActive(true);
+        notificationContent.text = text;
+        switch (type)
+        {
+            case NotificationType.info:
+                notificationContent.color = Color.white;
+                break;
+            case NotificationType.warning:
+                notificationContent.color = Color.yellow;
+                break;
+            case NotificationType.error:
+                notificationContent.color = Color.red;
+                break;
+        }
+
+        if (time != -1)
+        {
+            Invoke(nameof(CloseNotificationBar), time);
+        }
+    }
+
+    public void CloseNotificationBar()
+    {
+        gameObject.SetActive(false);
+        notificationContent.text = "";
+    }
+}
+
+public enum NotificationType : byte
+{
+    info,
+    warning,
+    error,
+}
+
+
