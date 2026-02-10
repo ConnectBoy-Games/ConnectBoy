33977eefb6ae14006ff703f52e1c8358a6f6c191
Oyale Salami <salamivictor15@gmail.com>
2026-02-10 13:56:29 +0100

    * Adds the dots and boxes icon * Adds the game description for the other games

---
+++ Assets/Scripts/UI Handlers/ActiveGamePanel.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class ActiveGamePanel : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] TMP_Text gameText;
+
+    public void CheckForExistingGame()
+    {
+        //TODO: Check if the player has an active game and load it if they do
+    }
+
+    public void GoToGame()
+    {
+
+    }
+
+    public void ForfeitGame()
+    {
+
+    }
+}
